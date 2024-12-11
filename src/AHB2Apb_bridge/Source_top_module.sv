701136855f20bffc43568ffa48bfe3b5f95601d6
Ahmed ibrahim <132453067+ahemd5@users.noreply.github.com>
2024-12-11 16:32:12 +0200

    Add files via upload

---
+++ src/AHB2Apb_bridge/Source_top_module.sv
@@ -0,0 +1,127 @@
+module source_top #(parameter ADDR_WIDTH = 32,
+	DATA_WIDTH = 32,
+	F_DEPTH = 4,
+	P_SIZE = 3					// p_size = log2(F_depth) + 1
+	)( 
+	input i_clk_src,
+	input i_rstn_src,
+	input i_src_sleep_req,
+	output o_src_sleep_ack,
+	// slave interface 
+    input  i_hready,
+    input  i_htrans,
+    input  [2:0] i_hsize,
+    input  i_hwrite,
+    input  [ADDR_WIDTH-1:0] i_haddr,
+    input  [DATA_WIDTH-1:0] i_hwdata,
+    input  i_hselx,
+	output o_hreadyout,
+    output o_hresp,
+    output [DATA_WIDTH-1:0] o_hrdata,
+	// async interface (sync inputs)
+	input  [P_SIZE-1:0] rsp_wr_ptr_sync,
+	input  [P_SIZE-1:0] req_rd_ptr_sync,
+	input  [DATA_WIDTH:0] rsp_FIFO_MEM_sync [F_DEPTH-1:0],
+	// outputs to synchronizer
+	output [DATA_WIDTH+ADDR_WIDTH+1:0] req_FIFO_MEM [F_DEPTH-1:0],
+	output [P_SIZE-1:0] req_wr_ptr,
+	output [P_SIZE-1:0] rsp_rd_ptr,
+	// sink
+	input sink_sleep_status,
+	output source_sleep_status
+	);
+	
+	// internal signals 
+	wire req_src_full;
+	wire req_src_empty;
+	wire rsp_src_empty;
+	wire [DATA_WIDTH+ADDR_WIDTH+1:0] o_packet_src;
+
+	wire req_src_wr_en;
+	wire rsp_src_rd_en;
+	wire src_fifos_reset;
+	wire [DATA_WIDTH:0] o_r_data_src;						// rsp fifo to controller
+	
+	wire o_ready_2slave;	
+	wire o_rd_valid;
+	wire [DATA_WIDTH-1:0] o_rd_data;
+	wire i_valid;
+	wire i_rd0_wr1;
+	wire [DATA_WIDTH-1:0] i_wr_data;
+	wire [ADDR_WIDTH-1:0] i_addr;
+	
+
+	source_controller #(.ADDR_WIDTH(ADDR_WIDTH),.DATA_WIDTH(DATA_WIDTH),.packet_width(ADDR_WIDTH+DATA_WIDTH+2)) 
+	u0_src_ctrl(
+	.i_clk_src(i_clk_src),
+	.i_rstn_src(i_rstn_src),
+	.i_src_sleep_req(i_src_sleep_req),
+	.sink_sleep_status(sink_sleep_status),
+	.i_read_packet(o_r_data_src),
+	.rd0_wr1(i_rd0_wr1),
+	.addr(i_addr),
+	.valid(i_valid),
+	.wr_data(i_wr_data),
+	.ready(o_ready_2slave),
+	.rd_data(o_rd_data),
+	.rd_valid(o_rd_valid),
+	.req_fifo_full(req_src_full),
+	.req_fifo_empty(req_src_empty),
+	.rsp_fifo_empty(rsp_src_empty),
+	.o_src_sleep_ack(o_src_sleep_ack),
+	.source_sleep_status(source_sleep_status),
+	.o_packet(o_packet_src),
+	.req_fifo_wr_en(req_src_wr_en),
+	.rsp_fifo_rd_en(rsp_src_rd_en),
+	.reset_flag(src_fifos_reset)
+	);
+	
+	Async_fifo_write #(.D_SIZE(DATA_WIDTH+ADDR_WIDTH+2),.F_DEPTH(F_DEPTH),.P_SIZE(P_SIZE))
+	u1_req_src_async_fifo(
+	.i_w_clk(i_clk_src),
+	.i_w_rstn(src_fifos_reset),
+	.i_w_inc(req_src_wr_en),
+	.i_w_data(o_packet_src),
+	.gray_rd_ptr_sync(req_rd_ptr_sync),
+	.o_full(req_src_full),
+	.o_empty(req_src_empty),
+	.FIFO_MEM(req_FIFO_MEM),
+	.gray_w_ptr(req_wr_ptr)
+	);
+	
+	Async_fifo_read #(.D_SIZE(DATA_WIDTH+1),.F_DEPTH(F_DEPTH),.P_SIZE(P_SIZE))
+	u2_rsp_src_async_fifo(
+	.i_r_clk(i_clk_src),
+	.i_r_rstn(src_fifos_reset),
+	.i_r_inc(rsp_src_rd_en),
+	.gray_w_ptr_sync(rsp_wr_ptr_sync),
+	.FIFO_MEM_sync(rsp_FIFO_MEM_sync),
+	.o_r_data(o_r_data_src),
+	.o_empty(rsp_src_empty),
+	.gray_rd_ptr(rsp_rd_ptr)
+	); 
+	
+	ahb_slave #(.DATA_WIDTH(DATA_WIDTH),.ADDR_WIDTH(ADDR_WIDTH)) 
+	u3_slave(
+	.i_clk_ahb(i_clk_src),
+	.i_rstn_ahb(i_rstn_src),
+	.i_hready(i_hready),
+	.i_htrans(i_htrans),
+	.i_hsize(i_hsize),
+	.i_hwrite(i_hwrite),
+	.i_haddr(i_haddr),
+	.i_hwdata(i_hwdata),
+	.i_hselx(i_hselx),
+	.i_ready(o_ready_2slave),
+	.i_rd_valid(o_rd_valid),
+	.i_rd_data(o_rd_data),
+	.o_hreadyout(o_hreadyout),
+	.o_hresp(o_hresp),
+	.o_hrdata(o_hrdata),
+	.o_valid(i_valid),
+	.o_rd0_wr1(i_rd0_wr1),
+	.o_wr_data(i_wr_data),
+	.o_addr(i_addr)
+	);
+	
+endmodule
---
+++ src/AHB2Apb_bridge/source_controller.v
@@ -0,0 +1,141 @@
+module source_controller #(parameter ADDR_WIDTH = 32,
+	DATA_WIDTH = 32, packet_width = 66 )( 
+	input wire 					i_clk_src,
+	input wire 					i_rstn_src,
+	input wire 					i_src_sleep_req,
+	input wire 					sink_sleep_status,
+	input wire [DATA_WIDTH:0]	i_read_packet,
+	// slave interface
+	input wire 					rd0_wr1,
+	input wire [ADDR_WIDTH-1:0] addr,
+	input wire 					valid,
+	input wire [DATA_WIDTH-1:0] wr_data,
+	output reg 					ready,
+	output reg [DATA_WIDTH-1:0] rd_data,
+	output reg					rd_valid,
+	// fifo
+	input wire 					req_fifo_full,
+	input wire 					req_fifo_empty,
+	input wire 					rsp_fifo_empty,
+	
+	output reg 					  o_src_sleep_ack,
+	output reg 					  source_sleep_status,
+	output reg [packet_width-1:0] o_packet,				// addr_width + data_width + 2 bit 
+	output reg 					  req_fifo_wr_en,
+	output reg 					  rsp_fifo_rd_en,
+	
+	output reg 					  reset_flag
+	);
+	
+	reg [packet_width-1:0] packet;
+	
+	// fsm 
+	parameter 	normal = 2'b00,
+				sleep = 2'b01,
+				idle = 2'b11;
+				
+	reg [1:0] current_state, next_state;
+		
+	always @(posedge i_clk_src or negedge i_rstn_src) begin
+        if (!i_rstn_src) begin
+			current_state <= normal;
+			reset_flag <= 0;	
+        end 
+		else begin 
+			current_state <= next_state ;	
+		end 
+    end
+	
+	always @(*) begin
+        case(current_state) 
+			normal : begin
+				if(i_src_sleep_req || sink_sleep_status) begin 
+					next_state = sleep;
+				end 
+				else begin 
+					next_state = normal;
+				end 
+			end
+			
+			sleep : begin
+				if (req_fifo_empty && rsp_fifo_empty) begin 
+					next_state = idle;
+				end 
+				else begin 
+					next_state = sleep;
+				end 
+			end
+			
+			idle : begin
+				if(!sink_sleep_status && !i_src_sleep_req) begin 
+					next_state = normal;
+				end 
+				else begin 
+					next_state = idle;
+				end 
+			end
+	
+		endcase
+    end
+	
+	always @(*) begin
+		req_fifo_wr_en = 0;
+		rsp_fifo_rd_en = 0;	
+		source_sleep_status = 0;
+		o_src_sleep_ack = 0;
+		o_packet = 0;
+		rd_data = 0;
+		rd_valid = 0;
+		ready = 1;
+		reset_flag = 1;
+		packet = {rd0_wr1, valid, addr, wr_data};
+		case(current_state) 
+			normal : begin
+				rsp_fifo_rd_en = (rsp_fifo_empty)? 0 : 1;
+				if(valid) begin
+					req_fifo_wr_en = 1;
+				end else begin
+					req_fifo_wr_en = 0;
+				end
+				o_packet = packet;
+ 				ready = (req_fifo_full)? 0 : 1;
+				
+				if(rsp_fifo_rd_en) begin 
+					rd_data = i_read_packet[DATA_WIDTH-1:0];
+					rd_valid = i_read_packet[DATA_WIDTH];
+				end else begin 
+					rd_data = 0;
+					rd_valid = 0;
+				end 
+			end
+			
+			sleep : begin
+				rsp_fifo_rd_en = rd_valid;
+				req_fifo_wr_en = 0; 
+				source_sleep_status = i_src_sleep_req;
+				ready = 0;
+				rd_data = i_read_packet[DATA_WIDTH-1:0];
+				rd_valid = i_read_packet[DATA_WIDTH];
+				if(i_src_sleep_req && rsp_fifo_empty && req_fifo_empty) begin
+					o_src_sleep_ack = 1;	
+					reset_flag = 0;
+				end 
+				else begin 
+					o_src_sleep_ack = 0;
+					reset_flag = 1;
+				end 
+				
+			end
+			
+			idle : begin
+				o_src_sleep_ack = 0;
+				ready = 0;
+				rsp_fifo_rd_en = 0;
+				req_fifo_wr_en = 0;
+				reset_flag = 0;
+				source_sleep_status = i_src_sleep_req; 
+			end
+		endcase
+    end
+endmodule
+	
